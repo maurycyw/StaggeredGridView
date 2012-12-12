4c2165f4710c8052b91dbde5630cdb1e5cdeba4f
maurycyw <maurycy@everyme.com>
2012-12-12 15:13:37 -0800

    license prep

--- src/com/origamilabs/library/views/StaggeredGridView.java
+++ src/com/origamilabs/library/views/StaggeredGridView.java
@@ -15,7 +15,7 @@ package com.origamilabs.library.views;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
- * modified by Muarycy Wojtowicz
+ * modified by Maurycy Wojtowicz
  * 
  */
 
